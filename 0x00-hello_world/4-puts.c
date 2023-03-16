473b52db56a733bae5a305b77f0198527de7725d
DouniaELMKADAM <doudi.elmkadam@gmail.com>
2023-03-16 16:56:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Puts
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-   puts "Programming is like building a multilingual puzzle"	
+   puts ("\"Programming is like building a multilingual puzzle");	
     return (0);
 }
