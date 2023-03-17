99502695a8513d2666873a071a5b8bd1b1774c57
DouniaELMKADAM <doudi.elmkadam@gmail.com>
2023-03-17 16:11:35 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Prints all possible combinations of two different digits,
- * in ascending order, separated by a comma followed by a space.
+ *        in ascending order, separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -12,15 +13,16 @@ int main(void)
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{	
+		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 			if (digit1 == 8 && digit2 == 9)
-				continue;	
-		putchar(',');
-		putchar(' ');
-           }
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
 	putchar('\n');
 
 	return (0);
