0143d626bd8c79fe12133b21361e54888f25ab82
DouniaELMKADAM <125449929+DouniaELMKADAM@users.noreply.github.com>
2023-03-31 00:23:50 +0000

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * infinite_add - adds two integers stored as strings...
  * @n1: first integer string to add
@@ -22,7 +21,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[size_r] = 0;
 	s1--;
 	s2--;
-
 	while (s2 != n2 - 1 && s1 != n1 - 1)
 	{
 		r[index] = *s2 - '0' + *s1 + carry;
@@ -85,5 +83,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		index--;
 		index2++;
 	}
+	
 	return (r);
 }
